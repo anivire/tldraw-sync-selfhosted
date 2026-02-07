7ac68838200c635e7432d83d311b82d1464b5ab6
Nikita Grichan <anivire.work@gmail.com>
2026-02-07 17:43:52 +0300

    fix: pass env ROOT_DOMAIN in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,32 @@
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
-	return {
-		plugins: [react(), tailwindcss()],
-		server: {
-			host: true,
-			allowedHosts: [
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      host: true,
+      allowedHosts: [
         // pass your domain
-				'tldraw.anivire.xyz',
-				'localhost',
-				'127.0.0.1',
-				'::1'
-			],
-			headers: {
-				// Override CSP for development
-				'Content-Security-Policy': "default-src 'self' http: https: data: blob: 'unsafe-inline'; img-src 'self' http: https: data: blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';"
-			},
-			proxy: {
-				'/api': {
-					target: 'http://localhost:3030',
-					changeOrigin: true,
-					ws: true,
-				},
-			},
-		},
-	}
-})
+        process.env.ROOT_DOMAIN ?? '',
+        'localhost',
+        '127.0.0.1',
+        '::1',
+      ],
+      headers: {
+        // Override CSP for development
+        'Content-Security-Policy':
+          "default-src 'self' http: https: data: blob: 'unsafe-inline'; img-src 'self' http: https: data: blob:; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';",
+      },
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3030',
+          changeOrigin: true,
+          ws: true,
+        },
+      },
+    },
+  };
+});
